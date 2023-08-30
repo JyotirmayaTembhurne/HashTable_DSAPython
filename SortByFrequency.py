2b95b777af320185afafbc9790b20f4dc2e66d8d
JyotirmayaTembhurne <84905370+JyotirmayaTembhurne@users.noreply.github.com>
2023-08-30 11:38:08 +0530

    Add files via upload
    The problem asked us to sort an array nums based on element frequency in ascending order. If multiple elements had same frequency , then we were to sort that in descending order. The code contains an example.

---
+++ SortByFrequency.py
@@ -0,0 +1,19 @@
+from operator import itemgetter
+
+nums = [2, 3, 1, 3, 2, 4, 4]
+Output = [1, 3, 3, 2, 2]
+# Explanation: '3' has a frequency of 1,
+# '1' has a frequency of 2, and
+# '2' has a frequency of 3.
+nums.sort(reverse=True)
+numsLen = len(nums)
+pair = dict()
+result = list()
+for i in range(numsLen):
+    pair[nums[i]] = nums.count(nums[i])
+pair = sorted(pair.items(), key=itemgetter(1))
+pairLen = len(pair)
+for item in pair:
+    for i in range(item[1]):
+        result.append(item[0])
+print(result)
